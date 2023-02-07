83f67b4a03d5f950a0e7c1963f38f7c0f459b3e7
CarlaBA <carla_belen_05@hotmail.com>
2023-02-07 18:43:50 -0300

    Update MetodosProducto.cs

---
+++ Metodos/MetodosProducto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_final
+{
+    internal static class MetodosProducto
+    {
+        public static string conexion = "Data Source = DESKTOP-2FTHB12\\MSSQLSERVER1; Initial Catalog = SistemaGestion; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+        public static List<Producto> ObtenerProducto(long idUsuario)
+        {
+            List<Producto> productos = new List<Producto>();
+            
+            using (SqlConnection con = new SqlConnection(conexion))
+            {
+                SqlCommand comando = new SqlCommand("SELECT * FROM Producto", con);
+                con.Open();
+
+                SqlDataReader reader = comando.ExecuteReader();
+                if(reader.HasRows)
+                {
+                    while(reader.Read())
+                    {
+                        Producto producto = new Producto();
+                        producto.Id = reader.GetInt64(0);
+                        producto.Descripciones = reader.GetString(1);
+                        producto.Costo = reader.GetDecimal(2);
+                        producto.PrecioVenta = reader.GetDecimal(3);
+                        producto.Stock = reader.GetInt32(4);
+                        producto.IdUsuario = reader.GetInt64(5);
+
+                        productos.Add(producto);
+                    }
+                } return productos;
+
+            }
+        }
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
